848ba9bbe1326402aafe1db62bc86a538382eed3
Ajeya5999 <ajeyasengar@gmail.com>
2023-06-01 16:00:39 +0530

    fifth commit: adding all views

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,7 +1,23 @@
 //For Sign-In Page
 
-module.exports.signIn = function(req, res){
+module.exports.signIn = function(req, res) {
     return res.render('sign_in', {
         title: "Sign In"
     });
 };
+
+// For Sign-Up Page
+
+module.exports.signUp = function(req, res) {
+    return res.render('sign_up', {
+        title: "Sign Up"
+    });
+}
+
+// For User-Profile Page
+
+module.exports.userProfile = function(req, res) {
+    return res.render('user_profile', {
+        title: "User Profile"
+    });
+}
--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/user_controller');
 
 router.get('/sign-in', userController.signIn); //For Sign-In Request
+router.get('/sign-up', userController.signUp); //For Sign-up Request
+router.get('/profile', userController.userProfile); //For User-Profile Request
 
 module.exports = router;
